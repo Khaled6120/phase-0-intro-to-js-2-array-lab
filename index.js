c58148f21628eb3f3639839efa60cc925fa9c201
Khaled Naes <86200305+Khaled6120@users.noreply.github.com>
2023-03-18 12:37:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 
 let cats = ["Milo", "Otis", "Garfield"];
 
-let destructivelyAppendCat = (name) => cats.push(name);
+let desAppendCat = (name) => cats.push(name);
 
-let destructivelyPrependCat = (name) => cats.unshift(name);
+let desPrependCat = (name) => cats.unshift(name);
 
-let destructivelyRemoveLastCat = () => cats.pop();
+let desRemoveLastCat = () => cats.pop();
 
-let destructivelyRemoveFirstCat = () => cats.shift();
+let desRemoveFirstCat = () => cats.shift();
 
-let appendCat = (name) => { return [...cats, name] }
+let appendCat = (name) => { 
+  return [...cats, name]
+}
 
-let prependCat = (name) => { return [name, ...cats] }
+let prependCat = (name) => { 
+  return [name, ...cats] 
+}
 
-let removeLastCat = () => { return cats.slice(0, 2) }
+let removeLastCat = () => { 
+  return cats.slice(0, 2)
+}
 
-let removeFirstCat = () => { return cats.slice(1) }
+let removeFirstCat = () => { 
+  return cats.slice(1)
+}
